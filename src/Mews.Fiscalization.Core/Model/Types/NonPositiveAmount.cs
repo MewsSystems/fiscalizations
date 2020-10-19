47e62058285a6e113992f7ff24d70e24d8806212
abdallahbeshi <abdallah.altrabeishi@mews.com>
2020-10-19 13:30:08 +0200

    Fixed.

---
+++ src/Mews.Fiscalization.Core/Model/Types/NonPositiveAmount.cs
@@ -0,0 +1,16 @@
+﻿namespace Mews.Fiscalization.Core.Model
+{
+    public class NonPositiveAmount : Amount
+    {
+        private static readonly DecimalLimitation Limitation = new DecimalLimitation(max: 0);
+        public NonPositiveAmount(decimal value)
+            : base(value, Limitation)
+        {
+        }
+
+        public static bool IsValid(decimal value)
+        {
+            return Amount.IsValid(value, Limitation);
+        }
+    }
+}
