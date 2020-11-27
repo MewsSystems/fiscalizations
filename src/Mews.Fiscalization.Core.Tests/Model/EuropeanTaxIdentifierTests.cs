d3290734f816f02a5d955956cb99676b1c15b8cd
abdallahbeshi <abdallah.altrabeishi@mews.com>
2020-11-27 12:07:52 +0100

    Introduced eu tax id type.

---
+++ src/Mews.Fiscalization.Core.Tests/Model/EuropeanTaxIdentifierTests.cs
@@ -0,0 +1,58 @@
+﻿using Mews.Fiscalization.Core.Model;
+using NUnit.Framework;
+using System;
+
+namespace Mews.Fiscalization.Core.Tests.Model
+{
+    [TestFixture]
+    public sealed class EuropeanTaxIdentifierTests
+    {
+        [Test]
+        [TestCase("AT", "U99999999")]
+        [TestCase("BE", "0999999999")]
+        [TestCase("BG", "999999999")]
+        [TestCase("CY", "99999999L")]
+        [TestCase("CZ", "99999999")]
+        [TestCase("DE", "999999999")]
+        [TestCase("DK", "99999999")]
+        [TestCase("EE", "999999999")]
+        [TestCase("GR", "999999999")]
+        [TestCase("ES", "X9999999X")]
+        [TestCase("FI", "99999999")]
+        [TestCase("FR", "XX999999999")]
+        [TestCase("GB", "999999999")]
+        [TestCase("HU", "99999999")]
+        [TestCase("IE", "9S99999L")]
+        [TestCase("IT", "99999999999")]
+        [TestCase("LT", "999999999")]
+        [TestCase("LU", "99999999")]
+        [TestCase("LV", "99999999999")]
+        [TestCase("MT", "99999999")]
+        [TestCase("NL", "999999999B99")]
+        [TestCase("PL", "9999999999")]
+        [TestCase("PT", "999999999")]
+        [TestCase("RO", "99")]
+        [TestCase("SE", "999999999999")]
+        [TestCase("SI", "99999999")]
+        [TestCase("SK", "9999999999")]
+        public void ValidTaxIdentifierPassesValidation(string countryCode, string taxIdentifier)
+        {
+            Assert.IsTrue(EuropeanTaxIdentifier.IsValid(countryCode, taxIdentifier), $"Tax identifier: {taxIdentifier}, must be valid for country code {countryCode}.");
+        }
+
+        [Test]
+        public void InvalidTaxIdentifierFailsValidation()
+        {
+            Assert.IsFalse(EuropeanTaxIdentifier.IsValid("ES", "ABC1234567"), "Invalid tax identifier shouldn't pass the validation.");
+
+            var invalidCountryCodeException = Assert.Throws<ArgumentException>(() => EuropeanTaxIdentifier.IsValid("INVALID", "99999"));
+            Assert.AreEqual(invalidCountryCodeException.Message, "Invalid or not implemented country code.");
+
+            var nullCountryCodeException = Assert.Throws<ArgumentException>(() => EuropeanTaxIdentifier.IsValid(null, "99999"));
+            Assert.AreEqual(nullCountryCodeException.Message, "Country code cannot be null or empty.");
+
+            var invalidTaxIdentifierException = Assert.Throws<ArgumentException>(() => EuropeanTaxIdentifier.IsValid("CZ", ""));
+            Assert.AreEqual(invalidTaxIdentifierException.Message, "Tax identifier cannot be null or empty.");
+        }
+    }
+}
---
+++ src/Mews.Fiscalization.Core/Model/Types/EuropeanTaxIdentifier.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Mews.Fiscalization.Core.Model
+{
+    public sealed class EuropeanTaxIdentifier
+    {
+        public EuropeanTaxIdentifier(string alpha2CountryCode, string taxIdentifier)
+        {
+            ValidateTaxIdentifier(alpha2CountryCode, taxIdentifier);
+            Check.Condition(IsValid(alpha2CountryCode, taxIdentifier), "Invalid tax identifier.");
+        }
+
+        public static bool IsValid(string alpha2CountryCode, string taxIdentifier)
+        {
+            ValidateTaxIdentifier(alpha2CountryCode, taxIdentifier);
+            return Regex.IsMatch(taxIdentifier, EuropeanTaxIdentifierPatterns[alpha2CountryCode]);
+        }
+
+        private static void ValidateTaxIdentifier(string alpha2CountryCode, string taxIdentifier)
+        {
+            Check.Condition(alpha2CountryCode.IsNotNullNorWhitespace(), "Country code cannot be null or empty.");
+            Check.Condition(EuropeanTaxIdentifierPatterns.ContainsKey(alpha2CountryCode), "Invalid or not implemented country code.");
+            Check.Condition(taxIdentifier.IsNotNullNorWhitespace(), "Tax identifier cannot be null or empty.");
+        }
+        
+        private static readonly IReadOnlyDictionary<string, string> EuropeanTaxIdentifierPatterns = new Dictionary<string, string>
+        {
+            { "AT", "(AT)?U[0-9]{8}" },
+            { "BE", "(BE)?0[0-9]{9}" },
+            { "BG", "(BG)?[0-9]{9,10}" },
+            { "CY", "(CY)?[0-9]{8}L" },
+            { "CZ", "(CZ)?[0-9]{8,10}" },
+            { "DE", "(DE)?[0-9]{9}" },
+            { "DK", "(DK)?[0-9]{8}" },
+            { "EE", "(EE)?[0-9]{9}" },
+            { "GR", "(EL|GR)?[0-9]{9}" },
+            { "ES", "(ES)?[0-9A-Z][0-9]{7}[0-9A-Z]" },
+            { "FI", "(FI)?[0-9]{8}" },
+            { "FR", "(FR)?[0-9A-Z]{2}[0-9]{9}" },
+            { "GB", "(GB)?([0-9]{9}([0-9]{3})?|[A-Z]{2}[0-9]{3})" },
+            { "HU", "(HU)?[0-9]{8}" },
+            { "IE", "(IE)?[0-9]S[0-9]{5}L" },
+            { "IT", "(IT)?[0-9]{11}" },
+            { "LT", "(LT)?([0-9]{9}|[0-9]{12})" },
+            { "LU", "(LU)?[0-9]{8}" },
+            { "LV", "(LV)?[0-9]{11}" },
+            { "MT", "(MT)?[0-9]{8}" },
+            { "NL", "(NL)?[0-9]{9}B[0-9]{2}" },
+            { "PL", "(PL)?[0-9]{10}" },
+            { "PT", "(PT)?[0-9]{9}" },
+            { "RO", "(RO)?[0-9]{2,10}" },
+            { "SE", "(SE)?[0-9]{12}" },
+            { "SI", "(SI)?[0-9]{8}" },
+            { "SK", "(SK)?[0-9]{10}" },
+        };
+    }
+}
