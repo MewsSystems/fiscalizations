8159e82b3f0697de2fa62f2aeaf58ff2c28e5e39
abdallahbeshi <abdallah.altrabeishi@mews.com>
2020-11-27 17:27:36 +0100

    split tests

---
+++ src/Mews.Fiscalization.Core.Tests/Model/CountryTests.cs
@@ -0,0 +1,29 @@
+﻿using Mews.Fiscalization.Core.Model;
+using NUnit.Framework;
+using System;
+
+namespace Mews.Fiscalization.Core.Tests.Model
+{
+    [TestFixture]
+    public sealed class CountryTests
+    {
+        [Test]
+        public void CreatingCountryWithInvalidCountryCodeFails()
+        {
+            Assert.Throws<ArgumentException>(() => new EuropeanCountry("US"));
+            Assert.Throws<ArgumentException>(() => new Country(""));
+            Assert.Throws<ArgumentNullException>(() => new Country(null));
+        }
+
+        [Test]
+        public void CreatingCountryWithValidCountryCodeSucceeds()
+        {
+            Assert.DoesNotThrow(() => new EuropeanCountry("CZ"));
+            Assert.DoesNotThrow(() => new Country("CZ"));
+            Assert.DoesNotThrow(() => new Country("US"));
+            Assert.IsTrue(EuropeanCountry.IsValid("CZ"));
+            Assert.IsTrue(Country.IsValid("CZ"));
+            Assert.IsTrue(Country.IsValid("US"));
+        }
+    }
+}
--- src/Mews.Fiscalization.Core.Tests/Model/TaxpayerIdentificationNumberTests.cs
+++ src/Mews.Fiscalization.Core.Tests/Model/TaxpayerIdentificationNumberTests.cs
@@ -71,24 +71,5 @@ namespace Mews.Fiscalization.Core.Tests.Model
             Assert.Throws<ArgumentNullException>(() => new TaxpayerIdentificationNumber(null, "ABC1234567"));
             Assert.Throws<ArgumentNullException>(() => new TaxpayerIdentificationNumber(country, null));
         }
-
-        [Test]
-        public void CreatingCountryWithInvalidCountryCodeFails()
-        {
-            Assert.Throws<ArgumentException>(() => new EuropeanCountry("US"));
-            Assert.Throws<ArgumentException>(() => new Country(""));
-            Assert.Throws<ArgumentNullException>(() => new Country(null));
-        }
-
-        [Test]
-        public void CreatingCountryWithValidCountryCodeSucceeds()
-        {
-            Assert.DoesNotThrow(() => new EuropeanCountry("CZ"));
-            Assert.DoesNotThrow(() => new Country("CZ"));
-            Assert.DoesNotThrow(() => new Country("US"));
-            Assert.IsTrue(EuropeanCountry.IsValid("CZ"));
-            Assert.IsTrue(Country.IsValid("CZ"));
-            Assert.IsTrue(Country.IsValid("US"));
-        }
     }
 }
