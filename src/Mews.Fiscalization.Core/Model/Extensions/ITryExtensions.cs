5c2d88d58c34dd3a961b9050a8702352ccc07fa5
KaliCZ <kalandrapavel@gmail.com>
2020-12-17 14:55:03 +0100

    Added funcsharp and some extensions

---
+++ src/Mews.Fiscalization.Core/Model/Extensions/ITryExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using FuncSharp;
+
+namespace Mews.Fiscalization.Core.Model
+{
+    public static class ITryExtensions
+    {
+        public static T Get<T, E>(this ITry<T, E> value)
+            where E : Exception
+        {
+            return value.Get(e => e);
+        }
+
+        public static ITry<T> Where<T>(this ITry<T> value, Func<T, bool> evaluator, Func<Unit, Exception> error)
+        {
+            return value.FlatMap(v => evaluator(v).ToTry(
+                t => v,
+                f => error(Unit.Value)
+            ));
+        }
+
+        public static ITry<T, E> Where<T, E>(this ITry<T, E> value, Func<T, bool> evaluator, Func<Unit, E> error)
+        {
+            return value.FlatMap(v => evaluator(v).ToTry(
+                t => v,
+                f => error(Unit.Value)
+            ));
+        }
+    }
+}
