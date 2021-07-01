7f627e5d39682a36d2096c860c18efa27dbbc38a
Abdallah Altrabeishi <51375082+abdallahbeshi@users.noreply.github.com>
2021-07-01 13:52:06 +0300

    Migrated Czech fiscalization tests to NUnit and fixed an issue with creating a the signedXML (#48)
    * Fixed czech fiscalization tests.
    
    * self
    
    * it works
    
    * self
    
    * self

--- src/Czechia/Mews.Fiscalizations.Czechia.Tests/UnitTests/CurrencyValueTests.cs
+++ src/Czechia/Mews.Fiscalizations.Czechia.Tests/UnitTests/CurrencyValueTests.cs
@@ -1,29 +1,27 @@
 ﻿using System;
 using Mews.Eet.Dto;
 using Mews.Eet.Extensions;
-using Xunit;
+using NUnit.Framework;
 
 namespace Mews.Eet.Tests.UnitTests
 {
     public class CurrencyValueTests
     {
-        [Fact]
+        [Test]
         public void IsDefinedWorks()
         {
             Assert.False((null as CurrencyValue).IsDefined());
             Assert.True(new CurrencyValue(100.00m).IsDefined());
         }
 
-        [Fact]
+        [Test]
         public void PrecisionIncreaseWorks()
         {
-            var ex = Record.Exception(() => new CurrencyValue(100m));
-            var ex1 = Record.Exception(() => new CurrencyValue(100.0m));
-            Assert.Null(ex);
-            Assert.Null(ex1);
+            Assert.DoesNotThrow(() => new CurrencyValue(100m));
+            Assert.DoesNotThrow(() => new CurrencyValue(100.0m));
         }
 
-        [Fact]
+        [Test]
         public void RangeCheckWorks()
         {
             Assert.Throws<ArgumentException>(() => new CurrencyValue(555555555555555555555555555.00m));
--- src/Czechia/Mews.Fiscalizations.Czechia.Tests/UnitTests/InputTests.cs
+++ src/Czechia/Mews.Fiscalizations.Czechia.Tests/UnitTests/InputTests.cs
@@ -1,80 +1,76 @@
 ﻿using System;
 using Mews.Eet.Dto.Identifiers;
-using Xunit;
+using NUnit.Framework;
 
 namespace Mews.Eet.Tests.UnitTests
 {
     public class InputTests
     {
-        [Fact]
+        [Test]
         public void TaxIdValidationWorks()
         {
             Assert.Throws<ArgumentException>(() => new TaxIdentifier("abcd"));
         }
 
-        [Fact]
+        [Test]
         public void RegistryIdValidationWorks()
         {
             Assert.Throws<ArgumentException>(() => new RegistryIdentifier(Guid.NewGuid().ToString()));
         }
 
 
-        [Fact]
+        [Test]
         public void BillNumberValidationWorks()
         {
             Assert.Throws<ArgumentException>(() => new BillNumber(Guid.NewGuid().ToString()));
         }
 
-        [Fact]
+        [Test]
         public void BillNumberValidationIsStrict()
         {
             Assert.Throws<ArgumentException>(() => new BillNumber("@@@@"));
         }
 
 
-        [Fact]
+        [Test]
         public void PremisesIdValidationWorks()
         {
             Assert.Throws<ArgumentException>(() => new PremisesIdentifier(0));
         }
 
-        [Fact]
+        [Test]
         public void TaxIdWorks()
         {
-            var ex = Record.Exception(() => new TaxIdentifier("CZ12345678"));
-            Assert.Null(ex);
+            Assert.DoesNotThrow(() => new TaxIdentifier("CZ12345678"));
         }
 
-        [Fact]
+        [Test]
         public void RegistryIdWorks()
         {
-            var ex = Record.Exception(() => new RegistryIdentifier("1"));
-            Assert.Null(ex);
+            Assert.DoesNotThrow(() => new RegistryIdentifier("1"));
         }
 
 
-        [Fact]
+        [Test]
         public void BillNumberWorks()
         {
-            var ex = Record.Exception(() => new BillNumber("2016020004"));
-            Assert.Null(ex);
+            Assert.DoesNotThrow(() => new BillNumber("2016020004"));
         }
 
 
-        [Fact]
+        [Test]
         public void PremisesIdWorks()
         {
-            var ex = Record.Exception(() => new PremisesIdentifier(1));
-            Assert.Null(ex);
+            Assert.DoesNotThrow(() => new PremisesIdentifier(1));
         }
 
-        [Fact]
+        [Test]
         public void IdentifierToStringWorks()
         {
             var number = "1234";
             var id = new BillNumber(number);
             var str = id.ToString();
-            Assert.Equal(number, str);
+            Assert.AreEqual(number, str);
         }
     }
 }
