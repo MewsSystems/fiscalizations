9b5dc0972bb431e3f863edb0ad991d704cd3d163
abdallahbeshi <abdallah.altrabeishi@mews.com>
2021-09-23 17:46:59 +0200

    self

---
+++ src/Germany/Mews.Fiscalizations.Germany.Tests/V2/TestFixture.cs
@@ -0,0 +1,19 @@
+﻿using Mews.Fiscalizations.Germany.V1;
+using Mews.Fiscalizations.Germany.V1.Model;
+using System;
+
+namespace Mews.Fiscalizations.Germany.Tests.V2
+{
+    public static class TestFixture
+    {
+        public static readonly Guid ClientId = new Guid(Environment.GetEnvironmentVariable("german_client_Id") ?? "INSERT_CLIENT_ID");
+        public static readonly Guid TssId = new Guid(Environment.GetEnvironmentVariable("german_tss_id") ?? "INSERT_TSS_ID");
+        public static readonly ApiKey ApiKey = ApiKey.Create(Environment.GetEnvironmentVariable("german_api_key") ?? "INSERT_API_KEY").Success.Get();
+        public static readonly ApiSecret ApiSecret = ApiSecret.Create(Environment.GetEnvironmentVariable("german_api_secret") ?? "INSERT_API_SECRET").Success.Get();
+
+        public static FiskalyClient GetFiskalyClient()
+        {
+            return new FiskalyClient(ApiKey, ApiSecret);
+        }
+    }
+}
