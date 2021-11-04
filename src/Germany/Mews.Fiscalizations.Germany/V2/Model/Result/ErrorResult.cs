0990538b59326878f5e9c3ae47c2b0244d01fcf2
abdallahbeshi <abdallah.altrabeishi@mews.com>
2021-11-04 16:05:52 +0100

    CR

--- src/Germany/Mews.Fiscalizations.Germany/V2/Model/Result/ErrorResult.cs
+++ src/Germany/Mews.Fiscalizations.Germany/V2/Model/Result/ErrorResult.cs
@@ -1,5 +1,6 @@
 ﻿using FuncSharp;
 using Mews.Fiscalizations.Core.Model;
+using Newtonsoft.Json;
 using System;
 
 namespace Mews.Fiscalizations.Germany.V2.Model
@@ -41,26 +42,26 @@ namespace Mews.Fiscalizations.Germany.V2.Model
                 "E_TX_UPSERT", _ => FiskalyError.InvalidTransactionOperation,
                 "E_TSS_DISABLED", _ => FiskalyError.InvalidTssOperation,
                 "E_TSS_NOT_INITIALIZED", _ => FiskalyError.InvalidTssOperation,
-                "E_TX_ILLEGAL_TYPE_CHANGE", _ => throw new InvalidOperationException($"Invalid request from the library {ToDebugObject(error)}."),
-                "E_TX_NO_TYPE_DEFINED", _ => throw new InvalidOperationException($"Invalid request from the library {ToDebugObject(error)}."),
-                "E_API_VERSION", _ => throw new InvalidOperationException($"Invalid request from the library {ToDebugObject(error)}."),
+                "E_TX_ILLEGAL_TYPE_CHANGE", _ => throw new InvalidOperationException($"Invalid request from the library {ToDebugString(error)}."),
+                "E_TX_NO_TYPE_DEFINED", _ => throw new InvalidOperationException($"Invalid request from the library {ToDebugString(error)}."),
+                "E_API_VERSION", _ => throw new InvalidOperationException($"Invalid request from the library {ToDebugString(error)}."),
                 "E_CLIENT_NOT_FOUND", _ => FiskalyError.InvalidClientId,
                 "E_TSS_NOT_FOUND", _ => FiskalyError.InvalidTssId,
                 "E_TSS_CONFLICT", _ => FiskalyError.TssCreationConflict,
                 "E_CLIENT_CONFLICT", _ => FiskalyError.ClientCreationConflict,
-                _ => throw new NotImplementedException($"{error.StatusCode}: {error.Code} is not implemented.")
+                _ => throw new NotImplementedException(ToDebugString(error))
             );
         }
 
-        private static object ToDebugObject(Dto.FiskalyErrorResponse errorResponse)
+        private static string ToDebugString(Dto.FiskalyErrorResponse errorResponse)
         {
-            return new
+            return JsonConvert.SerializeObject(new
             {
                 StatusCode = errorResponse.StatusCode,
                 Error = errorResponse.Error,
                 Code = errorResponse.Code,
                 Message = errorResponse.Message
-            };
+            });
         }
     }
 }
