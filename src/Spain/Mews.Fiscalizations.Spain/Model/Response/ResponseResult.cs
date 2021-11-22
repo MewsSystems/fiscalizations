33e31a40825e3e068cc1a79508517679d3feeca9
oleg-sokolansky <oleg.sokolansky@mews.com>
2021-11-22 17:46:07 +0100

    Update ResponseResult.cs

--- src/Spain/Mews.Fiscalizations.Spain/Model/Response/ResponseResult.cs
+++ src/Spain/Mews.Fiscalizations.Spain/Model/Response/ResponseResult.cs
@@ -1,21 +1,23 @@
 ﻿using FuncSharp;
-using Mews.Fiscalizations.Core.Model;
 
 namespace Mews.Fiscalizations.Spain.Model.Response
 {
     public static class ResponseResult
     {
-        public static ResponseResult<T> Success<T>(T result) where T : class
+        public static ResponseResult<T> Success<T>(T result)
+            where T : class
         {
             return new ResponseResult<T>(result);
         }
 
-        public static ResponseResult<T> Error<T>(ErrorResult error) where T : class
+        public static ResponseResult<T> Error<T>(ErrorResult error)
+            where T : class
         {
             return new ResponseResult<T>(error);
         }
 
-        public static ResponseResult<T> Error<T>(string code, string message) where T : class
+        public static ResponseResult<T> Error<T>(string code, string message)
+            where T : class
         {
             return new ResponseResult<T>(new ErrorResult(code, message));
         }
@@ -24,11 +26,13 @@ namespace Mews.Fiscalizations.Spain.Model.Response
     public class ResponseResult<TResult> : Coproduct2<TResult, ErrorResult>
         where TResult : class
     {
-        internal ResponseResult(TResult successResult = null) : base(successResult)
+        internal ResponseResult(TResult successResult = null)
+            : base(successResult)
         {
         }
 
-        internal ResponseResult(ErrorResult errorResult = null) : base(errorResult)
+        internal ResponseResult(ErrorResult errorResult = null)
+            : base(errorResult)
         {
         }
 
