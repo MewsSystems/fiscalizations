904e4a1e5b8aacd95ebeebf54d6bb16fdc45f0fe
oleg-sokolansky <oleg.sokolansky@mews.com>
2021-11-23 10:41:36 +0100

    make error result an exception

--- src/Spain/Mews.Fiscalizations.Spain/Model/Response/ErrorResult.cs
+++ src/Spain/Mews.Fiscalizations.Spain/Model/Response/ErrorResult.cs
@@ -1,16 +1,15 @@
-﻿namespace Mews.Fiscalizations.Spain.Model.Response
+﻿using System;
+
+namespace Mews.Fiscalizations.Spain.Model.Response
 {
-    public sealed class ErrorResult
+    public sealed class ErrorResult : Exception
     {
-        private ErrorResult(string code, string message)
+        private ErrorResult(string code, string message) : base(message)
         {
             Code = code;
-            Message = message;
         }
 
         public string Code { get; }
-
-        public string Message { get; }
 
         public static ErrorResult Create(string code, string message) => new ErrorResult(code, message);
     }
