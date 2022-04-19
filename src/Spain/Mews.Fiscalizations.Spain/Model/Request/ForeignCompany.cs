3cedbca2adaf0e16b49bb3065681024b258b1f24
abdallahbeshi <abdallah.altrabeishi@mews.com>
2022-04-19 15:45:42 +0200

    CR

--- src/Spain/Mews.Fiscalizations.Spain/Model/Request/ForeignCompany.cs
+++ src/Spain/Mews.Fiscalizations.Spain/Model/Request/ForeignCompany.cs
@@ -2,20 +2,16 @@
 {
     public class ForeignCompany
     {
-        public ForeignCompany(
-            Name name,
-            ForeignCompanyTaxpayerNumber taxpayerNumber,
-            ResidenceCountryIdentificatorType identificatiorType = ResidenceCountryIdentificatorType.NotSelected)
+        public ForeignCompany(Name name, ForeignCompanyTaxpayerNumber taxpayerNumber)
         {
             Name = name;
             TaxpayerNumber = taxpayerNumber;
-            IdentificatorType = identificatiorType;
         }
 
         public Name Name { get; }
 
         public ForeignCompanyTaxpayerNumber TaxpayerNumber { get; }
 
-        public ResidenceCountryIdentificatorType IdentificatorType { get; }
+        public ResidenceCountryIdentificatorType IdentificatorType => ResidenceCountryIdentificatorType.NotSelected;
     }
 }
