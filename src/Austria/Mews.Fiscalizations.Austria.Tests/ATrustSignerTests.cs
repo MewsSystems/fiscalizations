0f9facc65aaa98c06eaf11e4c21fd65044bc34fc
abdallahbeshi <abdallah.altrabeishi@mews.com>
2022-04-26 11:32:36 +0200

    Added A-Trust test account and enabled the tests back.

--- src/Austria/Mews.Fiscalizations.Austria.Tests/ATrustSignerTests.cs
+++ src/Austria/Mews.Fiscalizations.Austria.Tests/ATrustSignerTests.cs
@@ -11,8 +11,8 @@ namespace Mews.Fiscalizations.Austria.Tests
 {
     public class ATrustSignerTests
     {
-        public static readonly ATrustUserIdentifier UserId = new ATrustUserIdentifier("u123456789");
-        public static readonly string Password = "123456789";
+        public static readonly ATrustUserIdentifier UserId = new ATrustUserIdentifier(Environment.GetEnvironmentVariable("austrian_user_id") ?? "INSERT_USER_ID");
+        public static readonly string Password = Environment.GetEnvironmentVariable("austrian_password") ?? "INSERT_PASSWORD";
         public static readonly CertificateSerialNumber CertificateSerialNumber = new CertificateSerialNumber("-3667961875706356849");
         public static readonly JwsRepresentation JwsRepresentation = new JwsRepresentation("eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.WDFJeExVRlVNVjloT0RRME1URXpZaTFoTTJRM0xUUmxObU10T0RGak9DMDJOalU0TXpnMk9HVm1NelpmTTE4eU1ERTNMVEV5TFRFeVZERXlPalV6T2pVMlh6QXNNREJmTVRBd0xEQXdYekFzTURCZk1Dd3dNRjh3TERBd1h6ZzNMMnR2YW05RVYwUjNQVjh3TUVJd05qQkJNRUkwTWpFMlJUQXhSRFJmZVROVVp6TXlOV1Z0Y0UwOQ.6mzl1HSWmJyWaUG0pZlNuF29Eg9jocyXSuBxYWnwskE3fpVLd2PTIHG9ecBvQnCW3SokaMiEEgYN969Z4P7i0w");
         public static readonly string CertificateKey = "RCsRmHn5tkLQrRpiZq2ucwPpwvHJLiMgLvwrwEImddI=";
@@ -22,7 +22,6 @@ namespace Mews.Fiscalizations.Austria.Tests
             get { return new ATrustCredentials(user: UserId, password: Password); }
         }
 
-        [Ignore("Issue with test environment account, will re-enable once fixed.")]
         [Test]
         public void ATrustSignerWorks()
         {
