fa52aeaad981c0fbf95514c40f1dd43b359de125
Abdallah Altrabeishi <51375082+abdallahbeshi@users.noreply.github.com>
2022-07-04 18:22:05 +0200

    Introduced TicketBai invoice footer and original invoice models (#135)
    * Introduced TicketBai invoice footer and chain models
    
    * CR
    
    * self
    
    Co-authored-by: abdallahbeshi <abdallah.altrabeishi@mews.com>

---
+++ src/TicketBai/Mews.Fiscalizations.TicketBai/Model/Invoice/InvoiceFooter.cs
@@ -0,0 +1,21 @@
+﻿using FuncSharp;
+using Mews.Fiscalizations.Core.Model;
+
+namespace Mews.Fiscalizations.TicketBai.Model
+{
+    public sealed class InvoiceFooter
+    {
+        public InvoiceFooter(Software software, OriginalInvoiceInfo originalInvoiceInfo = null, String1To30 deviceSerialNumber = null)
+        {
+            Software = Check.IsNotNull(software, nameof(software));
+            OriginalInvoiceInfo = originalInvoiceInfo.ToOption();
+            DeviceSerialNumber = deviceSerialNumber.ToOption();
+        }
+
+        public Software Software { get; }
+
+        public IOption<OriginalInvoiceInfo> OriginalInvoiceInfo { get; }
+
+        public IOption<String1To30> DeviceSerialNumber { get; }
+    }
+}
---
+++ src/TicketBai/Mews.Fiscalizations.TicketBai/Model/Invoice/OriginalInvoiceInfo.cs
@@ -0,0 +1,25 @@
+﻿using FuncSharp;
+using Mews.Fiscalizations.Core.Model;
+using System;
+
+namespace Mews.Fiscalizations.TicketBai.Model
+{
+    public sealed class OriginalInvoiceInfo
+    {
+        public OriginalInvoiceInfo(String1To20 number, DateTime issueDate, String1To100 signature, String1To20 series = null)
+        {
+            Number = Check.IsNotNull(number, nameof(number));
+            IssueDate = Check.IsNotNull(issueDate, nameof(issueDate));
+            Signature = Check.IsNotNull(signature, nameof(signature));
+            Series = series.ToOption();
+        }
+
+        public String1To20 Number { get; }
+
+        public DateTime IssueDate { get; }
+
+        public String1To100 Signature { get; }
+
+        public IOption<String1To20> Series { get; }
+    }
+}
