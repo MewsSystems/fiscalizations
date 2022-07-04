9481c0f9814573bb94f931ad2ca5b250640e4b67
abdallahbeshi <abdallah.altrabeishi@mews.com>
2022-07-04 17:25:36 +0200

    Introduced TicketBai invoice footer and chain models

---
+++ src/TicketBai/Mews.Fiscalizations.TicketBai/Model/Invoice/InvoiceFooter.cs
@@ -0,0 +1,21 @@
+﻿using FuncSharp;
+using Mews.Fiscalizations.Core.Model;
+
+namespace Mews.Fiscalizations.TicketBai.Model
+{
+    public sealed class InvoiceFooter
+    {
+        public InvoiceFooter(Software software, PreviousInvoiceChain previousInvoiceChain = null, String1To30 deviceSerialNumber = null)
+        {
+            Software = Check.IsNotNull(software, nameof(software));
+            PreviousInvoiceChain = previousInvoiceChain.ToOption();
+            DeviceSerialNumber = deviceSerialNumber.ToOption();
+        }
+
+        public Software Software { get; }
+
+        public IOption<PreviousInvoiceChain> PreviousInvoiceChain { get; }
+
+        public IOption<String1To30> DeviceSerialNumber { get; }
+    }
+}
