20de5267b142bea695245c7f55c7bf649c955ff6
abdallahbeshi <abdallah.altrabeishi@mews.com>
2022-07-04 19:44:48 +0200

    Introduced TicketBai invoice models

---
+++ src/TicketBai/Mews.Fiscalizations.TicketBai/Model/Invoice/CorrectedInvoice.cs
@@ -0,0 +1,21 @@
+﻿using Mews.Fiscalizations.Core.Model;
+using System;
+
+namespace Mews.Fiscalizations.TicketBai.Model
+{
+    public sealed class CorrectedInvoice
+    {
+        public CorrectedInvoice(String1To20 series, String1To20 number, DateTime issueDate)
+        {
+            Series = series;
+            Number = number;
+            IssueDate = issueDate;
+        }
+
+        public String1To20 Series { get; }
+
+        public String1To20 Number { get; }
+
+        public DateTime IssueDate { get; }
+    }
+}
---
+++ src/TicketBai/Mews.Fiscalizations.TicketBai/Model/Invoice/CorrectingInvoice.cs
@@ -0,0 +1,20 @@
+﻿using FuncSharp;
+
+namespace Mews.Fiscalizations.TicketBai.Model
+{
+    public sealed class CorrectingInvoice
+    {
+        public CorrectingInvoice(CorrectingInvoiceCode code, CorrectingInvoiceType type, CorrectingInvoiceAmount amount = null)
+        {
+            Code = code;
+            Type = type;
+            Amount = amount.ToOption();
+        }
+
+        public CorrectingInvoiceCode Code { get; }
+
+        public CorrectingInvoiceType Type { get; }
+
+        public IOption<CorrectingInvoiceAmount> Amount { get; }
+    }
+}
