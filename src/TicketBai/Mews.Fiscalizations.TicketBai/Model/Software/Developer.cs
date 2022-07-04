f0a081b16555b236497f77ece22477868ae59541
Abdallah Altrabeishi <51375082+abdallahbeshi@users.noreply.github.com>
2022-07-04 16:34:55 +0200

    Introduced TicketBai software and developer models (#132)
    * Introduced TicketBai software and developer models
    
    * self
    
    Co-authored-by: abdallahbeshi <abdallah.altrabeishi@mews.com>

---
+++ src/TicketBai/Mews.Fiscalizations.TicketBai/Model/Software/Developer.cs
@@ -0,0 +1,20 @@
+﻿using FuncSharp;
+using Mews.Fiscalizations.Core.Model;
+
+namespace Mews.Fiscalizations.TicketBai.Model
+{
+    public sealed class Developer : Coproduct2<LocalDeveloper, ForeignDeveloper>
+    {
+        public Developer(LocalDeveloper local)
+            : base(local)
+        {
+            Check.IsNotNull(local, nameof(local));
+        }
+
+        public Developer(ForeignDeveloper foreign)
+            : base(foreign)
+        {
+            Check.IsNotNull(foreign, nameof(foreign));
+        }
+    }
+}
---
+++ src/TicketBai/Mews.Fiscalizations.TicketBai/Model/Software/Software.cs
@@ -0,0 +1,33 @@
+﻿using Mews.Fiscalizations.Core.Model;
+
+namespace Mews.Fiscalizations.TicketBai.Model
+{
+    public sealed class Software
+    {
+        private Software(String1To20 license, Developer developer, String1To120 name, String1To20 version)
+        {
+            License = Check.IsNotNull(license, nameof(license));
+            Developer = Check.IsNotNull(developer, nameof(developer));
+            Name = Check.IsNotNull(name, nameof(name));
+            Version = Check.IsNotNull(version, nameof(version));
+        }
+
+        public String1To20 License { get; }
+
+        public Developer Developer { get; }
+
+        public String1To120 Name { get; }
+
+        public String1To20 Version { get; }
+
+        public static Software LocalSoftwareDeveloper(TaxpayerIdentificationNumber nif, String1To20 license, String1To120 name, String1To20 version)
+        {
+            return new Software(license, new Developer(new LocalDeveloper(nif)), name, version);
+        }
+
+        public static Software ForeignSoftwareDeveloper(IdType idType, String1To20 id, String1To20 license, String1To120 name, String1To20 version, Country country = null)
+        {
+            return new Software(license, new Developer(new ForeignDeveloper(idType, id, country)), name, version);
+        }
+    }
+}
