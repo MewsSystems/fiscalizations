2bd975fbe96f37f713832daa66806bccaf839bf6
abdallahbeshi <abdallah.altrabeishi@mews.com>
2022-07-04 16:11:56 +0200

    Introduced TicketBai software and developer models

---
+++ src/TicketBai/Mews.Fiscalizations.TicketBai/Model/Software/LocalDeveloper.cs
@@ -0,0 +1,14 @@
+﻿using Mews.Fiscalizations.Core.Model;
+
+namespace Mews.Fiscalizations.TicketBai.Model
+{
+    public sealed class LocalDeveloper
+    {
+        public LocalDeveloper(TaxpayerIdentificationNumber nif)
+        {
+            Nif = Check.IsNotNull(nif, nameof(nif));
+        }
+
+        public TaxpayerIdentificationNumber Nif { get; }
+    }
+}
