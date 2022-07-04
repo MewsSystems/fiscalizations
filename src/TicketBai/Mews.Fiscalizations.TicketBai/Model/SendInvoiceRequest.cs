9bc6b081cc274980cba4cfacc639bd41f1ef67f7
abdallahbeshi <abdallah.altrabeishi@mews.com>
2022-07-04 20:18:14 +0200

    Introduced TicketBai invoice request and mapping.

---
+++ src/TicketBai/Mews.Fiscalizations.TicketBai/Model/SendInvoiceRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace Mews.Fiscalizations.TicketBai.Model
+{
+    public sealed class SendInvoiceRequest
+    {
+        public SendInvoiceRequest(Subject subject, Invoice invoice, InvoiceFooter invoiceFooter)
+        {
+            Subject = subject;
+            Invoice = invoice;
+            InvoiceFooter = invoiceFooter;
+        }
+
+        public Subject Subject { get; }
+
+        public Invoice Invoice { get; }
+
+        public InvoiceFooter InvoiceFooter { get; }
+    }
+}
---
+++ src/TicketBai/Mews.Fiscalizations.TicketBai/Model/Subject.cs
@@ -0,0 +1,35 @@
+﻿using FuncSharp;
+using Mews.Fiscalizations.Core.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mews.Fiscalizations.TicketBai.Model
+{
+    public sealed class Subject
+    {
+        private Subject(Issuer issuer, IEnumerable<Receiver> receivers, IssuerType? issuerType = null)
+        {
+            Issuer = issuer;
+            Receivers = receivers;
+            MultipleReceivers = Receivers.Count() > 1;
+            IssuerType = issuerType.ToOption();
+        }
+
+        public Issuer Issuer { get; }
+
+        public IEnumerable<Receiver> Receivers { get; }
+
+        public bool MultipleReceivers { get; }
+
+        public IOption<IssuerType> IssuerType { get; }
+
+        public static ITry<Subject, IEnumerable<Error>> Create(Issuer issuer, IEnumerable<Receiver> receivers, IssuerType? issuerType = null)
+        {
+            return Try.Aggregate(
+                ObjectValidations.NotNull(issuer),
+                receivers.ToTry(i => i.Count() >= 1 && i.Count() <= 100, _ => new Error($"{nameof(receivers)} count must be in range [1, 100].")),
+                (i, r) => new Subject(i, r, issuerType)
+            );
+        }
+    }
+}
