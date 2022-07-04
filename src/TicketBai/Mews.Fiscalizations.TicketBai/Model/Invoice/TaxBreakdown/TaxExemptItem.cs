c5874d1d21c6beac7839bc2556e8a5b1f30acb06
abdallahbeshi <abdallah.altrabeishi@mews.com>
2022-07-04 16:38:32 +0200

    fixed.

---
+++ src/TicketBai/Mews.Fiscalizations.TicketBai/Model/Invoice/TaxBreakdown/TaxExemptItem.cs
@@ -0,0 +1,17 @@
+﻿using Mews.Fiscalizations.Core.Model;
+
+namespace Mews.Fiscalizations.TicketBai.Model.Invoice
+{
+    public sealed class TaxExemptItem
+    {
+        public TaxExemptItem(Amount amount, CauseOfExemption cause)
+        {
+            Amount = Check.IsNotNull(amount, nameof(amount));
+            Cause = cause;
+        }
+
+        public Amount Amount { get; }
+
+        public CauseOfExemption Cause { get; }
+    }
+}
