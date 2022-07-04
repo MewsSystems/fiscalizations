0543c6ddf454ad1340a4afd8f632f81599b94b2a
abdallahbeshi <abdallah.altrabeishi@mews.com>
2022-07-04 20:30:35 +0200

    Introduced TicketBai response model

---
+++ src/TicketBai/Mews.Fiscalizations.TicketBai/Model/SendInvoiceResponse.cs
@@ -0,0 +1,49 @@
+﻿using FuncSharp;
+using System;
+using System.Collections.Generic;
+
+namespace Mews.Fiscalizations.TicketBai.Model
+{
+    public sealed class SendInvoiceResponse
+    {
+        public SendInvoiceResponse(
+            string xmlRequestContent,
+            string qrCodeUri,
+            string tbaiIdentifier,
+            DateTime received,
+            string state,
+            string description,
+            string stateExplanation,
+            string csv,
+            IEnumerable<SendInvoiceValidationResult> validationResults = null)
+        {
+            XmlRequestContent = xmlRequestContent;
+            QrCodeUri = qrCodeUri;
+            TBAIIdentifier = tbaiIdentifier;
+            Received = received;
+            State = state;
+            Description = description;
+            StateExplanation = stateExplanation;
+            CSV = csv.ToNonEmptyOption();
+            ValidationResults = validationResults.ToOption();
+        }
+
+        public string XmlRequestContent { get; }
+
+        public string QrCodeUri { get; }
+
+        public string TBAIIdentifier { get; }
+
+        public DateTime Received { get; }
+
+        public string State { get; } // TODO: enum?
+
+        public string Description { get; }
+
+        public string StateExplanation { get; }
+
+        public IOption<string> CSV { get; }
+
+        public IOption<IEnumerable<SendInvoiceValidationResult>> ValidationResults { get; }
+    }
+}
