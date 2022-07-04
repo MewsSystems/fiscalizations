8658feabe18b813bff964519d49dd729f46e01bc
abdallahbeshi <abdallah.altrabeishi@mews.com>
2022-07-04 17:35:25 +0200

    Introduced TicketBai receiver and issuer models.

---
+++ src/TicketBai/Mews.Fiscalizations.TicketBai/Model/Issuer.cs
@@ -0,0 +1,17 @@
+﻿using Mews.Fiscalizations.Core.Model;
+
+namespace Mews.Fiscalizations.TicketBai.Model
+{
+    public sealed class Issuer
+    {
+        public Issuer(TaxpayerIdentificationNumber nif, Name name)
+        {
+            Nif = Check.IsNotNull(nif, nameof(nif));
+            Name = Check.IsNotNull(name, nameof(name));
+        }
+
+        public TaxpayerIdentificationNumber Nif { get; }
+
+        public Name Name { get; }
+    }
+}
