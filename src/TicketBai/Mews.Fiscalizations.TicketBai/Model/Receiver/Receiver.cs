525150be716f11a393ac4f8fd4e4d96f5fee7677
Abdallah Altrabeishi <51375082+abdallahbeshi@users.noreply.github.com>
2022-07-04 18:08:00 +0200

    Introduced TicketBai receiver and issuer models. (#136)
    Co-authored-by: abdallahbeshi <abdallah.altrabeishi@mews.com>

---
+++ src/TicketBai/Mews.Fiscalizations.TicketBai/Model/Receiver/Receiver.cs
@@ -0,0 +1,30 @@
+﻿using FuncSharp;
+using Mews.Fiscalizations.Core.Model;
+
+namespace Mews.Fiscalizations.TicketBai.Model
+{
+    public sealed class Receiver : Coproduct2<LocalReceiver, ForeignReceiver>
+    {
+        public Receiver(LocalReceiver localReceiver)
+            : base(localReceiver)
+        {
+            Check.IsNotNull(localReceiver, nameof(localReceiver));
+        }
+
+        public Receiver(ForeignReceiver foreignReceiver)
+            : base(foreignReceiver)
+        {
+            Check.IsNotNull(foreignReceiver, nameof(foreignReceiver));
+        }
+
+        public static Receiver Local(TaxpayerIdentificationNumber nif, Name name, PostalCode postalCode, String1To250 address)
+        {
+            return new Receiver(new LocalReceiver(nif, name, postalCode, address));
+        }
+
+        public static Receiver Foreign(IdType idType, String1To20 id, Name name, PostalCode postalCode, String1To250 address, Country country = null)
+        {
+            return new Receiver(new ForeignReceiver(idType, id, name, postalCode, address, country));
+        }
+    }
+}
---
+++ src/TicketBai/Mews.Fiscalizations.TicketBai/Model/Receiver/ReceiverInfo.cs
@@ -0,0 +1,20 @@
+﻿using Mews.Fiscalizations.Core.Model;
+
+namespace Mews.Fiscalizations.TicketBai.Model
+{
+    public class ReceiverInfo
+    {
+        public ReceiverInfo(Name name, PostalCode postalCode, String1To250 address)
+        {
+            Name = Check.IsNotNull(name, nameof(name));
+            PostalCode = Check.IsNotNull(postalCode, nameof(postalCode));
+            Address = Check.IsNotNull(address, nameof(address));
+        }
+
+        public Name Name { get; }
+
+        public PostalCode PostalCode { get; }
+
+        public String1To250 Address { get; }
+    }
+}
