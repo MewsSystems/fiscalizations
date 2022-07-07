ed2139a4bd824c2924b85df5e1cabe0700b1c564
Abdallah Altrabeishi <51375082+abdallahbeshi@users.noreply.github.com>
2022-07-07 12:47:56 +0200

    Refactoring (#142)
    Co-authored-by: abdallahbeshi <abdallah.altrabeishi@mews.com>

---
+++ src/TicketBai/Mews.Fiscalizations.TicketBai/ServiceInfo.cs
@@ -0,0 +1,40 @@
+﻿using Mews.Fiscalizations.TicketBai.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mews.Fiscalizations.TicketBai
+{
+    public static class ServiceInfo
+    {
+        internal static IDVersionTicketBaiType1 Version { get; }
+
+        internal static Dictionary<Environment, Uri> InvoiceBaseUrls { get; }
+
+        internal static Dictionary<Environment, Uri> QrBaseUrls { get; }
+
+        internal static Uri RelativeSendInvoiceUri { get; }
+
+        internal static Uri RelativeQrCodeUri { get; }
+
+        internal static Encoding Encoding { get; }
+
+        static ServiceInfo()
+        {
+            Version = IDVersionTicketBaiType1.Item12;
+            InvoiceBaseUrls = new Dictionary<Environment, Uri>
+            {
+                [Environment.Test] = new Uri("https://tbai-z.prep.gipuzkoa.eus"),
+                [Environment.Production] = new Uri("https://tbai-z.egoitza.gipuzkoa.eus")
+            };
+            QrBaseUrls = new Dictionary<Environment, Uri>
+            {
+                [Environment.Test] = new Uri("https://tbai.prep.gipuzkoa.eus"),
+                [Environment.Production] = new Uri("https://tbai.egoitza.gipuzkoa.eus")
+            };
+            RelativeSendInvoiceUri = new Uri("sarrerak/alta/", UriKind.Relative);
+            RelativeQrCodeUri = new Uri("qr/", UriKind.Relative);
+            Encoding = Encoding.UTF8;
+        }
+    }
+}
