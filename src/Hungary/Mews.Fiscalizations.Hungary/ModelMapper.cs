88d163b5a0c578d506e3531e982e851218083cd9
abdallahbeshi <abdallah.altrabeishi@mews.com>
2022-09-13 13:43:41 +0200

    Extended the HU lib result with the xml request and response.

--- src/Hungary/Mews.Fiscalizations.Hungary/ModelMapper.cs
+++ src/Hungary/Mews.Fiscalizations.Hungary/ModelMapper.cs
@@ -2,74 +2,106 @@
 using Mews.Fiscalizations.Hungary.Utils;
 using System.Linq;
 using FuncSharp;
+using Mews.Fiscalizations.Core.Xml;
 
 namespace Mews.Fiscalizations.Hungary
 {
     internal static class ModelMapper
     {
-        internal static ResponseResult<ExchangeToken, ExchangeTokenErrorCode> MapExchangeToken(Dto.TokenExchangeResponse response, TechnicalUser user)
+        internal static ResponseResult<ExchangeToken, ExchangeTokenErrorCode> MapExchangeToken(
+            string requestXml,
+            string responseXml,
+            Dto.TokenExchangeResponse response,
+            TechnicalUser user)
         {
             try
             {
                 var decryptedToken = Aes.Decrypt(user.EncryptionKey.Value, response.encodedExchangeToken);
-                return new ResponseResult<ExchangeToken, ExchangeTokenErrorCode>(successResult: new ExchangeToken(
-                    value: decryptedToken,
-                    validFrom: response.tokenValidityFrom,
-                    validTo: response.tokenValidityTo
-                ));
+                return new ResponseResult<ExchangeToken, ExchangeTokenErrorCode>(
+                    requestXml: requestXml,
+                    responseXml: responseXml,
+                    successResult: new ExchangeToken(
+                        value: decryptedToken,
+                        validFrom: response.tokenValidityFrom,
+                        validTo: response.tokenValidityTo
+                    )
+                );
             }
             catch
             {
-                return new ResponseResult<ExchangeToken, ExchangeTokenErrorCode>(operationErrorResult: new ErrorResult<ExchangeTokenErrorCode>(
-                    errorCode: ExchangeTokenErrorCode.InvalidEncryptionKey
-                ));
+                return new ResponseResult<ExchangeToken, ExchangeTokenErrorCode>(
+                    requestXml: requestXml,
+                    responseXml: responseXml,
+                    operationErrorResult: new ErrorResult<ExchangeTokenErrorCode>(errorCode: ExchangeTokenErrorCode.InvalidEncryptionKey)
+                );
             }
         }
 
-        internal static ResponseResult<TaxPayerData, TaxPayerErrorCode> MapTaxPayerData(Dto.QueryTaxpayerResponse response)
+        internal static ResponseResult<TaxPayerData, TaxPayerErrorCode> MapTaxPayerData(
+            string requestXml,
+            string responseXml,
+            Dto.QueryTaxpayerResponse response)
         {
             if (response.taxpayerValidity)
             {
                 var addressItem = response.taxpayerData.taxpayerAddressList.First();
                 var taxPayerData = response.taxpayerData;
                 var taxNumberDetail = taxPayerData.taxNumberDetail;
-                return new ResponseResult<TaxPayerData, TaxPayerErrorCode>(successResult: new TaxPayerData(
-                    id: taxNumberDetail.taxpayerId,
-                    name: taxPayerData.taxpayerName,
-                    address: MapAddress(addressItem),
-                    vatCode: taxNumberDetail.vatCode,
-                    infoDate: response.infoDate,
-                    incorporationType: MapIncorporationType(taxPayerData.incorporation)
-                ));
+                return new ResponseResult<TaxPayerData, TaxPayerErrorCode>(
+                    requestXml: requestXml,
+                    responseXml: responseXml,
+                    successResult: new TaxPayerData(
+                        id: taxNumberDetail.taxpayerId,
+                        name: taxPayerData.taxpayerName,
+                        address: MapAddress(addressItem),
+                        vatCode: taxNumberDetail.vatCode,
+                        infoDate: response.infoDate,
+                        incorporationType: MapIncorporationType(taxPayerData.incorporation)
+                    )
+                );
             }
             else
             {
-                return new ResponseResult<TaxPayerData, TaxPayerErrorCode>(operationErrorResult: new ErrorResult<TaxPayerErrorCode>(
-                    errorCode: TaxPayerErrorCode.InvalidTaxPayer
-                ));
+                return new ResponseResult<TaxPayerData, TaxPayerErrorCode>(
+                    requestXml: requestXml,
+                    responseXml: responseXml,
+                    operationErrorResult: new ErrorResult<TaxPayerErrorCode>(errorCode: TaxPayerErrorCode.InvalidTaxPayer)
+                );
             }
         }
 
-        internal static ResponseResult<TransactionStatus, TransactionErrorCode> MapTransactionStatus(Dto.QueryTransactionStatusResponse response)
+        internal static ResponseResult<TransactionStatus, TransactionErrorCode> MapTransactionStatus(
+            string requestXml,
+            string responseXml,
+            Dto.QueryTransactionStatusResponse response)
         {
             var result = response.processingResults;
             if (result?.processingResult == null)
             {
-                return new ResponseResult<TransactionStatus, TransactionErrorCode>(operationErrorResult: new ErrorResult<TransactionErrorCode>(
-                    errorCode: TransactionErrorCode.InvalidTransactionId
-                ));
+                return new ResponseResult<TransactionStatus, TransactionErrorCode>(
+                    requestXml: requestXml,
+                    responseXml: responseXml,
+                    operationErrorResult: new ErrorResult<TransactionErrorCode>(errorCode: TransactionErrorCode.InvalidTransactionId)
+                );
             }
 
             return new ResponseResult<TransactionStatus, TransactionErrorCode>(
-                successResult: new TransactionStatus(
-                    invoiceStatuses: result.processingResult.Select(r => InvoiceStatus.Map(r))
-                )
+                requestXml: requestXml,
+                responseXml: responseXml,
+                successResult: new TransactionStatus(invoiceStatuses: result.processingResult.Select(r => InvoiceStatus.Map(r)))
             );
         }
 
-        internal static ResponseResult<string, ResultErrorCode> MapManageInvoice(Dto.ManageInvoiceResponse response)
+        internal static ResponseResult<string, ResultErrorCode> MapManageInvoice(
+            string requestXml,
+            string responseXml,
+            Dto.ManageInvoiceResponse response)
         {
-            return new ResponseResult<string, ResultErrorCode>(successResult: response.transactionId);
+            return new ResponseResult<string, ResultErrorCode>(
+                requestXml: requestXml,
+                responseXml: responseXml,
+                successResult: response.transactionId
+            );
         }
 
         private static IncorporationType MapIncorporationType(Dto.IncorporationType incorporationType)
--- src/Hungary/Mews.Fiscalizations.Hungary/Utils/Client.cs
+++ src/Hungary/Mews.Fiscalizations.Hungary/Utils/Client.cs
@@ -4,6 +4,7 @@ using Mews.Fiscalizations.Hungary.Models;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Mews.Fiscalizations.Hungary.Utils
 {
@@ -18,27 +19,33 @@ namespace Mews.Fiscalizations.Hungary.Utils
             Environment = environment;
         }
 
-        internal async Task<ResponseResult<TResult, TCode>> ProcessRequestAsync<TRequest, TDto, TResult, TCode>(string endpoint, TRequest request, Func<TDto, ResponseResult<TResult, TCode>> successFunc)
+        internal async Task<ResponseResult<TResult, TCode>> ProcessRequestAsync<TRequest, TDto, TResult, TCode>(
+            string endpoint,
+            TRequest request,
+            Func<TDto, string, string, ResponseResult<TResult, TCode>> successFunc)
             where TRequest : class
             where TDto : class
             where TResult : class
             where TCode : struct
         {
-            var httpResponse = await SendRequestAsync(endpoint, request);
-            return await DeserializeAsync(httpResponse, successFunc);
+            var parameters = new XmlSerializationParameters(namespaces: ServiceInfo.XmlNamespace.ToEnumerable());
+            var xmlRequest = XmlSerializer.Serialize(request, parameters);
+
+            var httpResponse = await SendRequestAsync(endpoint, xmlRequest);
+            return await DeserializeAsync(httpResponse, xmlRequest, successFunc);
         }
 
-        private Task<HttpResponseMessage> SendRequestAsync<TRequest>(string endpoint, TRequest request)
-            where TRequest : class
+        private Task<HttpResponseMessage> SendRequestAsync(string endpoint, XmlElement requestXml)
         {
-            var parameters = new XmlSerializationParameters(namespaces: ServiceInfo.XmlNamespace.ToEnumerable());
-            var xml = XmlSerializer.Serialize(request, parameters);
-            var content = new StringContent(xml.OuterXml, ServiceInfo.Encoding, "application/xml");
+            var content = new StringContent(requestXml.OuterXml, ServiceInfo.Encoding, "application/xml");
             var uri = new Uri(ServiceInfo.BaseUrls[Environment], $"{ServiceInfo.RelativeServiceUrl}{endpoint}");
             return HttpClient.PostAsync(uri, content);
         }
 
-        private async Task<ResponseResult<TResult, TCode>> DeserializeAsync<TDto, TResult, TCode>(HttpResponseMessage response, Func<TDto, ResponseResult<TResult, TCode>> successFunc)
+        private async Task<ResponseResult<TResult, TCode>> DeserializeAsync<TDto, TResult, TCode>(
+            HttpResponseMessage response,
+            XmlElement xmlRequest,
+            Func<TDto, string, string, ResponseResult<TResult, TCode>> successFunc)
             where TDto : class
             where TResult : class
             where TCode : struct
@@ -46,12 +53,12 @@ namespace Mews.Fiscalizations.Hungary.Utils
             var content = await response.Content.ReadAsStringAsync();
             if (response.IsSuccessStatusCode)
             {
-                return successFunc(XmlSerializer.Deserialize<TDto>(content));
+                return successFunc(XmlSerializer.Deserialize<TDto>(content), XmlSerializer.Serialize(xmlRequest).OuterXml, content);
             }
             else
             {
                 var errorResult = XmlSerializer.Deserialize<Dto.GeneralErrorResponse>(content);
-                return new ResponseResult<TResult, TCode>(generalErrorMessage: ErrorResult<TCode>.Map(errorResult));
+                return new ResponseResult<TResult, TCode>(requestXml: xmlRequest.OuterXml, responseXml: content, generalErrorMessage: ErrorResult<TCode>.Map(errorResult));
             }
         }
     }
