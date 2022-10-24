01a3f6fa56477b54f9b9e751358e1230e0a8974f
abdallahbeshi <abdallah.altrabeishi@mews.com>
2022-10-24 16:53:31 +0200

    WIP

--- src/Basque/Mews.Fiscalizations.Basque/TicketBaiSignature.cs
+++ src/Basque/Mews.Fiscalizations.Basque/TicketBaiSignature.cs
@@ -8,73 +8,77 @@ namespace Mews.Fiscalizations.Basque
 {
     internal static class TicketBaiSignature
     {
+        private static readonly string NamespaceUri = "http://uri.etsi.org/01903/v1.3.2#";
+
+        private static readonly string XadesPrefix = "xades";
+
+        private static readonly string DsigPrefix = "dsig";
+
         static TicketBaiSignature()
         {
             Id = Guid.NewGuid();
         }
 
-        public static Guid Id { get; }
-
-        public static string NamespaceUri => "http://uri.etsi.org/01903/v1.3.2#";
+        private static Guid Id { get; }
 
         public static void Sign(XmlDocument xmlDoc, X509Certificate2 certificate, Region region)
         {
             var signedXml = new XAdESSignedXml(xmlDoc);
             signedXml.Signature.Id = $"id-{Id}";
-            signedXml.Signature.SignedInfo.CanonicalizationMethod = SignedXml.XmlDsigExcC14NTransformUrl;
+            signedXml.Signature.SignedInfo.CanonicalizationMethod = SignedXml.XmlDsigC14NTransformUrl;
             signedXml.SignedInfo.SignatureMethod = SignedXml.XmlDsigRSASHA256Url;
 
-            var qualifyingPropertiesRoot = xmlDoc.CreateElement("xades", "QualifyingProperties", NamespaceUri);
+            var qualifyingPropertiesRoot = xmlDoc.CreateElement(XadesPrefix, "QualifyingProperties", NamespaceUri);
             qualifyingPropertiesRoot.SetAttribute("Target", $"#id-{Id}");
 
-            var signaturePropertiesRoot = xmlDoc.CreateElement("xades", "SignedProperties", NamespaceUri);
-            signaturePropertiesRoot.SetAttribute("Id", $"xades-id-{Id}");
+            var signaturePropertiesRoot = xmlDoc.CreateElement(XadesPrefix, "SignedProperties", NamespaceUri);
+            signaturePropertiesRoot.SetAttribute("Id", $"{XadesPrefix}-id-{Id}");
 
-            var signedSignatureProperties = xmlDoc.CreateElement("xades", "SignedSignatureProperties", NamespaceUri);
+            var signedSignatureProperties = xmlDoc.CreateElement(XadesPrefix, "SignedSignatureProperties", NamespaceUri);
 
-            var timestamp = xmlDoc.CreateElement("xades", "SigningTime", NamespaceUri);
+            var timestamp = xmlDoc.CreateElement(XadesPrefix, "SigningTime", NamespaceUri);
             timestamp.InnerText = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.fffZ");
 
-            var signaturePolicyIdentifier = xmlDoc.CreateElement("xades", "SignaturePolicyIdentifier", NamespaceUri);
-            var signaturePolicyId = xmlDoc.CreateElement("xades", "SignaturePolicyId", NamespaceUri);
-            var sigPolicyId = xmlDoc.CreateElement("xades", "SigPolicyId", NamespaceUri);
+            var signaturePolicyIdentifier = xmlDoc.CreateElement(XadesPrefix, "SignaturePolicyIdentifier", NamespaceUri);
+            var signaturePolicyId = xmlDoc.CreateElement(XadesPrefix, "SignaturePolicyId", NamespaceUri);
+            var sigPolicyId = xmlDoc.CreateElement(XadesPrefix, "SigPolicyId", NamespaceUri);
 
-            var sigPolicyIdentifier = xmlDoc.CreateElement("xades", "Identifier", NamespaceUri);
+            var sigPolicyIdentifier = xmlDoc.CreateElement(XadesPrefix, "Identifier", NamespaceUri);
             sigPolicyIdentifier.InnerText = "urn:ticketbai:dss:policy:1";
 
-            var sigPolicyDescription = xmlDoc.CreateElement("xades", "Description", NamespaceUri);
+            var sigPolicyDescription = xmlDoc.CreateElement(XadesPrefix, "Description", NamespaceUri);
             sigPolicyDescription.InnerText = "TicketBAI sinadura-politika / Politica de firma TicketBAI";
 
-            var sigPolicyHash = xmlDoc.CreateElement("xades", "SigPolicyHash", NamespaceUri);
+            var sigPolicyHash = xmlDoc.CreateElement(XadesPrefix, "SigPolicyHash", NamespaceUri);
 
-            var sigPolicyHashDigestMethod = xmlDoc.CreateElement("dsig", "DigestMethod", NamespaceUri);
+            var sigPolicyHashDigestMethod = xmlDoc.CreateElement(DsigPrefix, "DigestMethod", NamespaceUri);
             sigPolicyHashDigestMethod.SetAttribute("Algorithm", SignedXml.XmlDsigSHA256Url);
 
-            var sigPolicyHashDigestValue = xmlDoc.CreateElement("dsig", "DigestValue", NamespaceUri);
+            var sigPolicyHashDigestValue = xmlDoc.CreateElement(DsigPrefix, "DigestValue", NamespaceUri);
             sigPolicyHashDigestValue.InnerText = region.Match(
                 Region.Gipuzkoa, _ => "vSe1CH7eAFVkGN0X2Y7Nl9XGUoBnziDA5BGUSsyt8mg=",
                 Region.Araba, _ => "4Vk3uExj7tGn9DyUCPDsV9HRmK6KZfYdRiW3StOjcQA="
             );
 
-            var sigPolicyQualifiers = xmlDoc.CreateElement("xades", "SigPolicyQualifiers", NamespaceUri);
-            var sigPolicyQualifier = xmlDoc.CreateElement("xades", "SigPolicyQualifier", NamespaceUri);
+            var sigPolicyQualifiers = xmlDoc.CreateElement(XadesPrefix, "SigPolicyQualifiers", NamespaceUri);
+            var sigPolicyQualifier = xmlDoc.CreateElement(XadesPrefix, "SigPolicyQualifier", NamespaceUri);
 
-            var SPURI = xmlDoc.CreateElement("xades", "SPURI", NamespaceUri);
-            SPURI.InnerText = region.Match(
+            var Spuri = xmlDoc.CreateElement(XadesPrefix, "SPURI", NamespaceUri);
+            Spuri.InnerText = region.Match(
                 Region.Gipuzkoa, _ => "https://www.gipuzkoa.eus/ticketbai/sinadura",
                 Region.Araba, _ => "https://ticketbai.araba.eus/tbai/sinadura/"
             );
 
-            var signedDataObjectProperties = xmlDoc.CreateElement("xades", "SignedDataObjectProperties", NamespaceUri);
-            var dataObjectFormat = xmlDoc.CreateElement("xades", "DataObjectFormat", NamespaceUri);
+            var signedDataObjectProperties = xmlDoc.CreateElement(XadesPrefix, "SignedDataObjectProperties", NamespaceUri);
+            var dataObjectFormat = xmlDoc.CreateElement(XadesPrefix, "DataObjectFormat", NamespaceUri);
             dataObjectFormat.SetAttribute("ObjectReference", $"#Id-{Id}");
 
-            var mimeType = xmlDoc.CreateElement("xades", "MimeType", NamespaceUri);
+            var mimeType = xmlDoc.CreateElement(XadesPrefix, "MimeType", NamespaceUri);
             mimeType.InnerText = "application/octet-stream";
 
             signedSignatureProperties.AppendChild(timestamp);
             sigPolicyQualifiers.AppendChild(sigPolicyQualifier);
-            sigPolicyQualifier.AppendChild(SPURI);
+            sigPolicyQualifier.AppendChild(Spuri);
             signaturePolicyIdentifier.AppendChild(signaturePolicyId);
             signaturePolicyId.AppendChild(sigPolicyId);
             sigPolicyId.AppendChild(sigPolicyIdentifier);
@@ -115,6 +119,7 @@ namespace Mews.Fiscalizations.Basque
                 SerialNumber = certificate.SerialNumber
             };
             certificateData.AddIssuerSerial(issuerSerial.IssuerName, issuerSerial.SerialNumber);
+            certificateData.AddSubjectName(certificate.Subject);
 
             var keyInfo = new KeyInfo();
             keyInfo.AddClause(certificateData);
@@ -126,18 +131,17 @@ namespace Mews.Fiscalizations.Basque
             var reference1 = new Reference
             {
                 Type = "",
-                Uri = $"",
+                Uri = "",
                 Id = $"Id-{Id}"
             };
             reference1.AddTransform(new XmlDsigEnvelopedSignatureTransform());
-            reference1.AddTransform(new XmlDsigExcC14NTransform());
 
             var reference2 = new Reference
             {
                 Type = "http://uri.etsi.org/01903#SignedProperties",
                 Uri = $"#xades-id-{Id}"
             };
-            reference2.AddTransform(new XmlDsigExcC14NTransform());
+            reference2.AddTransform(new XmlDsigC14NTransform());
 
             signedXml.AddReference(reference1);
             signedXml.AddReference(reference2);
