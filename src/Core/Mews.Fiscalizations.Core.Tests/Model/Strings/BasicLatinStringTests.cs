b77459ee905d4da0170afacef1e756d31fab036f
Abdallah Altrabeishi <51375082+abdallahbeshi@users.noreply.github.com>
2023-03-13 14:53:08 +0100

    Introduced a method that converts a string to Basic latin standard. (#189)
    Co-authored-by: abdallahbeshi <abdallah.altrabeishi@mews.com>

---
+++ src/Core/Mews.Fiscalizations.Core.Tests/Model/Strings/BasicLatinStringTests.cs
@@ -0,0 +1,22 @@
+﻿using Mews.Fiscalizations.Core.Model;
+using NUnit.Framework;
+
+namespace Mews.Fiscalizations.Core.Tests.Model
+{
+    [TestFixture]
+    public sealed class BasicLatinStringTests
+    {
+        [Test]
+        [TestCase(null, null)]
+        [TestCase("", "")]
+        [TestCase("   ", "   ")]
+        [TestCase("abcdefghijklmnopqrstuvwxyz0123456789", "abcdefghijklmnopqrstuvwxyz0123456789")]
+        [TestCase("!@#$%^&**()_-", "!@#$%^&**()_-")]
+        [TestCase("Euro € sign", "Euro  sign")]
+        [TestCase("€€€", "")]
+        public void ToBasicLatinStringSucceeds(string input, string expectedOutput)
+        {
+            Assert.AreEqual(expectedOutput, input.ToBasicLatin());
+        }
+    }
+}
