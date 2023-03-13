9aa2710591fa850d38cf4982055aeba5d6870a89
abdallahbeshi <abdallah.altrabeishi@mews.com>
2023-03-13 14:45:18 +0100

    Introduced a method that converts a string to Basic latin standard.

--- src/Core/Mews.Fiscalizations.Core/Model/Extensions/StringExtensions.cs
+++ src/Core/Mews.Fiscalizations.Core/Model/Extensions/StringExtensions.cs
@@ -1,4 +1,10 @@
-﻿namespace Mews.Fiscalizations.Core.Model
+﻿using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using FuncSharp;
+
+namespace Mews.Fiscalizations.Core.Model
 {
     public static class StringExtensions
     {
@@ -10,6 +16,25 @@
         public static bool IsNullOrWhitespace(this string s)
         {
             return string.IsNullOrEmpty(s) || string.IsNullOrEmpty(s.Trim());
+        }
+
+        public static string StripDiacritics(this string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                return s;
+            }
+
+            var simpleChars = s.Normalize(NormalizationForm.FormD).Where(c => CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark);
+            return new string(simpleChars.ToArray());
+        }
+
+        public static string ToBasicLatin(this string value)
+        {
+            return string.IsNullOrWhiteSpace(value).Match(
+                t => value,
+                f => Regex.Replace(value.StripDiacritics(), @"[^\p{IsBasicLatin}]+", "")
+            );
         }
     }
 }
