1e890384dc97d6153002ec29d33028ac4d234318
abdallahbeshi <abdallah.altrabeishi@mews.com>
2025-01-24 14:02:21 +0100

    Added handling of srv4pos error responses.

---
+++ src/Sweden/Mews.Fiscalizations.Sweden/DTOs/Srv4posBasicErrorResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Mews.Fiscalizations.Sweden.DTOs;
+
+public sealed class Srv4posBasicErrorResponse
+{
+    [JsonPropertyName("error")]
+    public string Error { get; set; }
+}
---
+++ src/Sweden/Mews.Fiscalizations.Sweden/Models/Srv4posErrorType.cs
@@ -0,0 +1,12 @@
+﻿namespace Mews.Fiscalizations.Sweden.Models;
+
+public enum Srv4posErrorType
+{
+    ServerSideError = 0,
+    InvalidRequest = 1,
+    InvalidCorporateId = 2,
+    InvalidCashRegisterName = 3,
+    InvalidFieldFormat = 4,
+    NonUniqueCashRegisterNameOrCorporateId = 5,
+    UnknownError = 6
+}
