da776f31773da385979dbf73b6f5272b4ee06ae4
Abdallah Al Trabeishi <51375082+abdallahbeshi@users.noreply.github.com>
2025-04-10 11:34:31 +0200

    Refactor Infrasec client and tests: remove ignored test, enhance docs (#267)
    * Refactor Infrasec client and tests: remove ignored test, enhance documentation, and update method signatures for optional parameters
    
    * temp
    
    * temp
    
    * test
    
    * self

--- src/Sweden/Mews.Fiscalizations.Sweden/IInfrasecClient.cs
+++ src/Sweden/Mews.Fiscalizations.Sweden/IInfrasecClient.cs
@@ -13,7 +13,7 @@ public interface IInfrasecClient
     /// <param name="requestId">Id identifying the request. If set to null, we will send a random number between 100000 and 999999</param>
     /// <param name="cancellationToken">A cancellation token to cancel the request</param>
     /// <returns>The response from the Infrasec API or an exception if the request fails</returns>
-    Task<Try<StatusEnrollmentResponse, Exception>> GetStatusCcuAsync(StatusEnrollmentData data, NonEmptyString applicationId, int? requestId, CancellationToken cancellationToken);
+    Task<Try<StatusEnrollmentResponse, Exception>> GetStatusCcuAsync(StatusEnrollmentData data, NonEmptyString applicationId, int? requestId = null, CancellationToken cancellationToken = default);
 
     /// <summary>
     /// Enrolls a new cash register. Request and responses use xml.
@@ -23,7 +23,7 @@ public interface IInfrasecClient
     /// <param name="requestId">Id identifying the request. If set to null, we will send a random number between 100000 and 999999</param>
     /// <param name="cancellationToken">A cancellation token to cancel the request</param>
     /// <returns>The response from the Infrasec API or an exception if the request fails</returns>
-    Task<Try<NewEnrollmentResponse, Exception>> EnrollCcuAsync(NewEnrollmentData data, NonEmptyString applicationId, int? requestId, CancellationToken cancellationToken);
+    Task<Try<NewEnrollmentResponse, Exception>> EnrollCcuAsync(NewEnrollmentData data, NonEmptyString applicationId, int? requestId = null, CancellationToken cancellationToken = default);
 
     /// <summary>
     /// Accepts receipt VAT information and returns a control code.
@@ -33,5 +33,5 @@ public interface IInfrasecClient
     /// <param name="requestId">Id identifying the request. If set to null, we will send a new Guid.</param>
     /// <param name="cancellationToken">A cancellation token to cancel the request.</param>
     /// <returns>The response from the Infrasec API or an exception if the request fails.</returns>
-    Task<Try<TransactionResponse, Exception>> SendTransactionAsync(TransactionData data, NonEmptyString applicationId, Guid? requestId, CancellationToken cancellationToken);
+    Task<Try<TransactionResponse, Exception>> SendTransactionAsync(TransactionData data, NonEmptyString applicationId, Guid? requestId = null, CancellationToken cancellationToken = default);
 }
