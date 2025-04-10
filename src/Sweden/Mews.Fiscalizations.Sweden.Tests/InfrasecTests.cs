df1afa9a085798cd3bb88da5a8922c0ea24d6485
abdallahbeshi <abdallah.altrabeishi@mews.com>
2025-04-10 14:21:19 +0200

    self

--- src/Sweden/Mews.Fiscalizations.Sweden.Tests/InfrasecTests.cs
+++ src/Sweden/Mews.Fiscalizations.Sweden.Tests/InfrasecTests.cs
@@ -14,7 +14,8 @@ namespace Mews.Fiscalizations.Sweden.Tests
         );
 
         private static readonly X509Certificate2 EnrollmentSigningCertificate = new(
-            rawData: Convert.FromBase64String(System.Environment.GetEnvironmentVariable("infrasec_enrollment_signing_certificate_data") ?? "INSERT_INFRASEC_ENROLLMENT_SIGNING_CERTIFICATE_DATA")
+            rawData: Convert.FromBase64String(System.Environment.GetEnvironmentVariable("infrasec_enrollment_signing_certificate_data") ?? "INSERT_INFRASEC_ENROLLMENT_SIGNING_CERTIFICATE_DATA"),
+            password: "NoKeyPassword" // :D
         );
 
         private static readonly X509Certificate2 TransactionCertificate = new(
