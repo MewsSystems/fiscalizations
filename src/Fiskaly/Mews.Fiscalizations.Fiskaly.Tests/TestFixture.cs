3054e77ee7d56a9ad80413c7d1714f751b399a1e
sebas-banzas <sebastian.banzas@mews.com>
2025-05-07 08:37:53 +0200

    made class static

--- src/Fiskaly/Mews.Fiscalizations.Fiskaly.Tests/TestFixture.cs
+++ src/Fiskaly/Mews.Fiscalizations.Fiskaly.Tests/TestFixture.cs
@@ -1,6 +1,6 @@
 namespace Mews.Fiscalizations.Fiskaly.Tests;
 
-public class TestFixture
+public static class TestFixture
 {
     public static readonly string SignESApiKey = Environment.GetEnvironmentVariable("fiskaly_signes_api_key") ?? "INSERT_API_KEY";
     public static readonly string SignESApiSecret = Environment.GetEnvironmentVariable("fiskaly_signes_api_secret") ?? "INSERT_API_SECRET";
